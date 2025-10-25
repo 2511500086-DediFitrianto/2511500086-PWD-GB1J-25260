3f383ea1d87518fadd590eb29b3b6355301ea6a1
2511500086-DediFitrianto <2511500086@mahasiswa.atmaluhur.ac.id>
2025-10-25 10:59:24 +0700

    tess

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,3 +1,6 @@
 document.getElementById("menuToggle").addEventListener("click", function () {
     document.querySelector("nav").classList.toggle("active");
 });
+
+let nama = prompt ("siapa nama kamu?")
+alert("halo, " + dedi + "!")
