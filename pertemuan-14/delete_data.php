22df11617743b5d4b8291380b15e0352bf199fd2
Dedifitrianto-2511500086 <2511500086@mahasiswa.atmaluhur.ac.id>
2026-01-12 16:59:50 +0700

    Menyelesaikan try out UAS

--- pertemuan-14/delete_data.php
+++ pertemuan-14/delete_data.php
@@ -3,32 +3,26 @@
   require __DIR__ . '/koneksi.php';
   require_once __DIR__ . '/fungsi.php';
 
-  $NIM = filter_input(INPUT_GET, 'NIM', FILTER_VALIDATE_INT, [
-    'options' => ['min_range' => 1]
-  ]);
+  /* LOGIKA 1: PENGHAPUSAN MAHASISWA (NIM)
+    Jika terdapat parameter 'nim' di URL
+  */
+  if (isset($_GET['NIM'])) {
+    $NIM = bersihkan($_GET['NIM']);
 
-  if (!$NIM) {
-    $_SESSION['flash_error1'] = 'Data Tidak Valid.';
+    if (empty($NIM)) {
+        $_SESSION['flash_error1'] = 'NIM tidak valid.';
+        redirect_ke('read_biodata.php');
+    }
+
+    $stmt = mysqli_prepare($conn, "DELETE FROM tbl_biodata WHERE NIM = ?");
+    mysqli_stmt_bind_param($stmt, "s", $NIM);
+
+    if (mysqli_stmt_execute($stmt)) {
+        $_SESSION['flash_sukses1'] = 'Data mahasiswa berhasil dihapus.';
+    } else {
+        $_SESSION['flash_error1'] = 'Gagal menghapus data mahasiswa.';
+    }
+
+    mysqli_stmt_close($stmt);
     redirect_ke('read_biodata.php');
   }
-
-  $stmt = mysqli_prepare($conn, "DELETE FROM tbl_biodata 
-                                WHERE NIM = ?");
-  if (!$stmt) {
-    $_SESSION['flash_error1'] = 'Terjadi kesalahan sistem (prepare gagal).';
-    redirect_ke('read_biodata.php');
-  }
-
-  mysqli_stmt_bind_param($stmt, "s", $NIM);
-
-  if (mysqli_stmt_execute($stmt)) { #jika berhasil, kosongkan old value
-    $_SESSION['flash_sukses1'] = 'Terima kasih, data Anda sudah dihapus.';
-  } else { #jika gagal, simpan kembali old value dan tampilkan error umum
-    $_SESSION['flash_error1'] = 'Data gagal dihapus. Silakan coba lagi.';
-  }
-  #tutup statement
-  mysqli_stmt_close($stmt);
-
-  redirect_ke('read_biodata.php');
-  
-  ?>
